bdccac0ab5f360439a750cf76d8edac719063f16
Steve Blaurock <sblaurock@gmail.com>
2017-05-07 23:36:30 -0400

    Display improvements, more efficient API endpoint.

--- index.js
+++ index.js
@@ -21,12 +21,23 @@ const addCommas = (string) => {
 let previousPriceData = {};
 let priceDataHistory = {};
 let previousExchange = null;
-const writeToStdout = (priceData) => {
-  const sortedExchanges = _.keys(priceData).sort();
-  const longestExchangeLength = sortedExchanges.sort((a, b) => { return b.length - a.length; })[0].length;
-
+let previousCurrency = null;
+let statusOutput = '';
+let apiFailure = false;
+const writeToStdout = (priceData, allowance) => {
   process.stdout.write('\033c');
   process.stdout.write('\n');
+
+  // Ensure we've not exhausted API limits
+  if (!priceData && _.keys(previousPriceData).length()) {
+    priceData = previousPriceData;
+    apiFailure = true;
+  } else if (!priceData) {
+    return process.stdout.write(`${colors.red(' ⚠ API limit has been reached')}\n\n`);
+  }
+
+  const sortedExchanges = _.keys(priceData).sort();
+  const longestExchangeLength = sortedExchanges.sort((a, b) => { return b.length - a.length; })[0].length;
 
   _.forEach(sortedExchanges, (exchange) => {
     const sortedMarkets = _.keys(priceData[exchange]).sort();
@@ -35,6 +46,7 @@ const writeToStdout = (priceData) => {
       const currencyA = market.substr(0, 3);
       const currencyB = market.substr(3, 3);
       let exchangeOutput = '';
+      let currencyOutput = '';
       let changeOutput = '';
       let historyOutput = '';
       let historyChangeOutput = '';
@@ -47,13 +59,21 @@ const writeToStdout = (priceData) => {
         exchangeOutput = colors.bold.white(leftPad('', longestExchangeLength + 3)) + leftPad('', options.app.padding);
       }
 
+      // Show currency name
+      if (previousCurrency !== currencyA) {
+        currencyOutput = `${currencyA.toUpperCase()}`;
+        previousCurrency = currencyA;
+      } else {
+        currencyOutput = leftPad('', 3);
+      }
+
       // Show percent change in last 24 hours
       if (priceData[exchange][market].price.change.percentage > 0) {
         changeOutput = colors.green(`▲ ${rightPad((priceData[exchange][market].price.change.percentage * 100).toFixed(2).toString() + '%', 6)}`);
       } else if (priceData[exchange][market].price.change.percentage < 0) {
         changeOutput = colors.red(`▼ ${rightPad(((priceData[exchange][market].price.change.percentage * 100).toFixed(2) * -1).toString() + '%', 6)}`);
       } else {
-        changeOutput = `- ${rightPad((priceData[exchange][market].price.change.percentage * 100).toFixed(2).toString() + '%', 6)}%`;
+        changeOutput = `- ${rightPad((priceData[exchange][market].price.change.percentage * 100).toFixed(2).toString() + '%', 6)}`;
       }
 
       // Show history of price updates
@@ -80,11 +100,28 @@ const writeToStdout = (priceData) => {
         }
       }
 
-      process.stdout.write(exchangeOutput + `${currencyA.toUpperCase()}` + leftPad('', options.app.padding) + `${leftPad(addCommas(priceData[exchange][market].price.last.toFixed(2)), 8)} ${currencyB.toUpperCase()} ` + `${changeOutput}` + leftPad('', options.app.padding) + `${(priceDataHistory[exchange + market] || '') && priceDataHistory[exchange + market].slice(-1 * options.app.history.length).join('')}` + ` ${colors.grey(historyChangeOutput)}` + `\n`);
+      // Show request status
+      if (
+        options.app.status.enabled &&
+        allowance.remaining < 100000000
+      ) {
+        if (!apiFailure) {
+          statusOutput = `${colors.yellow(' ⚠ API limit is close to being reached')}\n`;
+        } else {
+          statuOutput = `${colors.red(' ⚠ API limit has been reached')}\n`;
+        }
+      } else {
+        apiFailure = false;
+        statusOutput = '';
+      }
+
+      process.stdout.write(exchangeOutput + currencyOutput + leftPad('', options.app.padding) + `${leftPad(addCommas(priceData[exchange][market].price.last.toFixed(2)), 10)} ${currencyB.toUpperCase()} ` + `${changeOutput}` + ` ${(priceDataHistory[exchange + market] || '') && priceDataHistory[exchange + market].slice(-1 * options.app.history.length).join('')}` + ` ${colors.grey(historyChangeOutput)}` + `\n`);
     });
 
     process.stdout.write('\n');
   });
+
+  process.stdout.write(`${statusOutput}`);
 
   previousExchange = null;
   previousPriceData = priceData;
@@ -92,24 +129,27 @@ const writeToStdout = (priceData) => {
 
 // Retrieve pricing information from endpoint
 const retrieveMarketData = () => {
-  let leftToResolve = options.markets.length;
   let priceData = {};
 
-  options.markets.forEach((market) => {
-    needle.get(`https://api.cryptowat.ch/markets/${market}/summary`, (error, response) => {
-      if (!error && response.statusCode == 200) {
-        const [exchange, marketName] = market.split('/');
+  needle.get('https://api.cryptowat.ch/markets/summaries', (error, response) => {
+    if (!error && response.statusCode === 200) {
+      _.forEach(response.body.result, (data, market) => {
+        if (options.markets.indexOf(market) === -1) {
+          return;
+        }
+
+        const [exchange, marketName] = market.split(':');
 
         priceData[exchange] = priceData[exchange] || {};
-        priceData[exchange][marketName] = response.body && response.body.result;
-      }
+        priceData[exchange][marketName] = response.body && response.body.result[market];
+      });
 
-      leftToResolve--;
-
-      if (!leftToResolve) {
-        writeToStdout(priceData);
+      if (priceData) {
+        writeToStdout(priceData, response.body.allowance);
       }
-    });
+    } else if (response.statuscode === 429) {
+      writeToStdout(null);
+    }
   });
 };
 
